4e1ad2d408009f91dee0c5270f3485f138457d8d
Adrian Connolly <amjconnolly@gmail.com>
2015-08-14 15:25:51 +0900

    complete first draft of enemy class

--- app/src/main/java/com/example/android/tadhggame/Enemy.java
+++ app/src/main/java/com/example/android/tadhggame/Enemy.java
@@ -6,15 +6,23 @@ import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.view.Display;
+import android.view.WindowManager;
 
 import java.util.Random;
 
 /**
- * Created by adrian on 13/08/15.
+ * Enenmy class
+ * Has a static spawn() function that randomly creates and places new Enemy characters on the screen
+ * <p>
+ * Extends: Sprite
+ * <p>
+ * Date     Rev  Author       Description
+ * =======  ===  ===========  ===========================
+ * 15.8.14    0  A. Connolly  Initial implementation
  */
 public class Enemy extends Sprite {
 
-    //constants
+    //Enemy types
     final public static int SHOE = 0;
     final public static int WAN = 1;
     final public static int NYAN = 2;
@@ -27,19 +35,18 @@ public class Enemy extends Sprite {
     final public static int NORMAL = 0;
     final public static int GHOST = 1;
     final public static int EXPLODED = 2;
-    final private static Random rand = new Random();
-
-    //constants
-    final private int xVel = -10;
 
     //member variable
+    final private static Random rand = new Random(); //static random number generator
     private int mType;
     private Context mContext;
     private int mState = NORMAL; //default to normal
-    private int lastDrawTime;
-    private double amplitude;
+    private long lastDrawTime;
+    private int amplitude;
     private double phase;
     private double frequency;
+    private double widthCoefficient;
+    private int initialY;
 
     //member drawables
     private Drawable img;
@@ -51,112 +58,134 @@ public class Enemy extends Sprite {
         init(c);
     }
 
-    public Enemy(int x, int y, int width, int height, Context c){
-        super(x, y, width, height);
-        init(c);
-    }
+    public static Enemy spawn(Context c, int state, int w, int h) {
+        Enemy enemy = new Enemy(c);
+        enemy.setWidth(w);
+        enemy.setHeight(h);
 
-    private void init(Context c){
-        this.setType(rand.nextInt(7));
-        mContext=c;
-        this.loadDrawables();
-        amplitude=100+rand.nextDouble()*100;
-        frequency=rand.nextDouble()*10;
-        phase=rand.nextDouble()*Math.PI/2;
-    }
+        //get width of display
+        WindowManager wm = (WindowManager) enemy.getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display d = wm.getDefaultDisplay();
+        Point p = new Point();
+        d.getSize(p);
 
-    public static Enemy spawn(Context c, int state, int w, int h, int screenW, int screenH){
-        Enemy enemy;
-        int x,y;
-        x=screenW-w;
-        y=rand.nextInt(screenH-h);
-        enemy = new Enemy(x, y, w, h, c);
+        enemy.setX(p.x - w);
+        enemy.setY(enemy.getAmplitude() + rand.nextInt(p.y - h - 2 * enemy.getAmplitude()));
         enemy.setState(state);
+        enemy.setWidthCoefficient(Math.PI * 6 / (double) p.x);
         return enemy;
     }
 
-    public void draw(Canvas c){
+    private double getWidthCoefficient() {
+        return widthCoefficient;
+    }
+
+    private void setWidthCoefficient(double d) {
+        widthCoefficient = d;
+    }
+
+    private void init(Context c) {
+        this.setType(rand.nextInt(7));
+        mContext = c;
+        this.loadDrawables();
+        amplitude = (int) (100 + rand.nextDouble() * 100);
+        frequency = rand.nextDouble() * 10;
+        phase = rand.nextDouble() * Math.PI / 2;
+        lastDrawTime = System.currentTimeMillis();
+        setInitialY(getY());
+        setDx(-10);
+    }
+
+    private int getInitialY() {
+        return initialY;
+    }
+
+    private void setInitialY(int y) {
+        initialY = y;
+    }
+
+    private Context getContext() {
+        return mContext;
+    }
+
+    public int getAmplitude() {
+        return this.amplitude;
+    }
+
+    public void draw(Canvas c) {
         Drawable d;
-        switch(mState){
+        switch (mState) {
             case NORMAL:
-                d=img;
+                d = img;
                 break;
             case GHOST:
-                d=ghost;
+                d = ghost;
                 break;
-            case EXPLODED:
-                d=popped;
+            default:
+                d = popped;
                 break;
         }
         d.setBounds(
                 this.getX(),
                 this.getY(),
-                this.getX()+this.getWidth(),
-                this.getY()+this.getHeight()
+                this.getX() + this.getWidth(),
+                this.getY() + this.getHeight()
         );
         d.draw(c);
     }
 
-    private void loadDrawables(){
-        popped = ContextCompat.getDrawable(mContext, R.id.img_pop);
-        ghost = ContextCompat.getDrawable(mContext, R.id.img_ghost);
+    private void loadDrawables() {
+        popped = ContextCompat.getDrawable(mContext, R.drawable.img_pop);
+        ghost = ContextCompat.getDrawable(mContext, R.drawable.img_ghost);
         switch (mType) {
             case APPLE:
-                img = ContextCompat.getDrawable(mContext, R.id.img_apple);
+                img = ContextCompat.getDrawable(mContext, R.drawable.img_apple);
                 break;
 
             case WAN:
-                img = ContextCompat.getDrawable(mContext, R.id.img_wan);
+                img = ContextCompat.getDrawable(mContext, R.drawable.img_wan);
                 break;
 
             case NYAN:
-                img = ContextCompat.getDrawable(mContext, R.id.img_nyan);
+                img = ContextCompat.getDrawable(mContext, R.drawable.img_nyan);
                 break;
 
             case BANANA:
-                img = ContextCompat.getDrawable(mContext, R.id.img_banana);
+                img = ContextCompat.getDrawable(mContext, R.drawable.img_banana);
                 break;
 
             case SHOE:
-                img = ContextCompat.getDrawable(mContext, R.id.img_shoe);
+                img = ContextCompat.getDrawable(mContext, R.drawable.img_shoe);
                 break;
 
             case FISH:
-                img = ContextCompat.getDrawable(mContext, R.id.img_fish);
+                img = ContextCompat.getDrawable(mContext, R.drawable.img_fish);
+                break;
+
+            case PUMPKIN:
+                img = ContextCompat.getDrawable(mContext, R.drawable.img_pumpkin);
                 break;
         }
     }
 
-    public void updatePhysics(int timeNow) {
-        int dt = timeNow-lastDrawTime;
-        int newX,newY;
-        newX=this.getX()+xVel*dt;
+    public void updatePhysics(long timeNow) {
+        long dt = timeNow - lastDrawTime;
+        int newX, newY;
+        newX = this.getX() + (int) (getDx() * dt);
 
         switch (mState) {
-            case NORMAL:
-                switch (mType) {
-                    case APPLE:
-                        newY=(int)(this.getY()+amplitude*Math.sin(frequency*((double)newX)+phase));
-                        break;
+            default:
+                newY = (int) (this.getInitialY() +
+                        amplitude * Math.sin(frequency * ((double) newX * getWidthCoefficient()) + phase));
+                break;
 
-                    case WAN:
-                        break;
-
-                    case NYAN:
-                        break;
-
-                    case BANANA:
-                        break;
-
-                    case SHOE:
-                        break;
-
-                    case PUMPKIN:
-                        break;
-                }
             case GHOST:
+                newY = (int) (this.getInitialY() +
+                        amplitude * Math.sin(frequency * ((double) newX * getWidthCoefficient()) + phase * rand.nextInt(10)));
                 break;
         }
+        this.setX(newX);
+        this.setY(newY);
     }
 
     public int getType() {
